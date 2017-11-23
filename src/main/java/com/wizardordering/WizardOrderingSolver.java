0a182237355ebda98b168415b29a548414abf5be
Denny Hung <dennyhl@berkeley.edu>
2017-11-22 23:28:20 -0800

    Input files added

--- src/main/java/com/wizardordering/WizardOrderingSolver.java
+++ src/main/java/com/wizardordering/WizardOrderingSolver.java
@@ -1,4 +1,24 @@
 package main.java.com.wizardordering;
 
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ISolver;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class WizardOrderingSolver {
+    private ISolver solver;
+    private String inFileName;
+    private String outFileName;
+    private Set<String> wizardList;
+
+
+    public WizardOrderingSolver(String inFileName, String outFileName) {
+        this.solver = SolverFactory.newDefault();
+        this.inFileName = inFileName;
+        this.outFileName = outFileName;
+        this.wizardList = new HashSet<>();
+    }
+
+   // public
 }
