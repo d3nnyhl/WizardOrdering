f79de66eebdb55894a51aa9f6b2488780e4102fa
Denny Hung <dennyhl@berkeley.edu>
2017-11-22 23:11:21 -0800

    Java SAT solver imported

---
+++ src/main/java/com/wizardordering/WizardOrdering.java
@@ -0,0 +1,12 @@
+package main.java.com.wizardordering;
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
+
+
+public class WizardOrdering {
+
+}
