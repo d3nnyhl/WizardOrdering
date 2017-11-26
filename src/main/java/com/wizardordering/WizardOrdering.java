a468dfc16fb08c9b957ced911c1c998c24f3c4b6
Denny Hung <dennyhl@berkeley.edu>
2017-11-26 04:20:53 -0800

    Pre-processing done

--- src/main/java/com/wizardordering/WizardOrdering.java
+++ src/main/java/com/wizardordering/WizardOrdering.java
@@ -1,36 +1,43 @@
 package main.java.com.wizardordering;
 
-import org.sat4j.core.VecInt;
-import org.sat4j.minisat.SolverFactory;
-import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.IProblem;
-import org.sat4j.specs.ISolver;
-import org.sat4j.specs.TimeoutException;
+import edu.princeton.cs.algs4.Stopwatch;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class WizardOrdering {
     private static final String INPUT_FILES_PATH = "./src/main/resources/phase2_inputs/";
     private static final String STAFF_INPUT_PATH = "./src/main/resources/Staff_Inputs/";
     private static final String INPUT_FILE_EXTENSION = ".in";
-    private static Set<WizardOrderingSolver> solvers = new HashSet<>();
+    private static List<WizardOrderingSolver> solvers = new ArrayList<>();
+    private static List<WizardOrderingSolver> staffInputSolvers =  new ArrayList<>();
 
     private static void solveAll() {
-        for(WizardOrderingSolver solver: solvers) {
+        Stopwatch watch = new Stopwatch();
+        for (WizardOrderingSolver solver: solvers) {
+            solver.preProcess();
             solver.run();
+            solver.postProcess();
         }
+        System.out.println("All files solved in: " + watch.elapsedTime() + " s\n");
     }
 
+    private static void solveAllStaff() {
+        for (WizardOrderingSolver solver: staffInputSolvers) {
+            solver.preProcess();
+            solver.run();
+            solver.postProcess();
+        }
+    }
     private static void start() {
         File inputFilesFolder = new File(INPUT_FILES_PATH);
         File[] listOfSubFolders = inputFilesFolder.listFiles();
         for (File subfolder : listOfSubFolders) {
-            System.out.println(subfolder.getName());
             File[] listOfInputFiles = subfolder.listFiles();
+            Arrays.sort(listOfInputFiles);
             for (File inputFile : listOfInputFiles) {
                 String fileName = inputFile.getName();
                 if (inputFile.isFile() && fileName.endsWith(INPUT_FILE_EXTENSION)) {
@@ -40,9 +47,20 @@ public class WizardOrdering {
         }
     }
 
-
+    private static void startStaffFiles() {
+        File inputFilesFolder = new File(STAFF_INPUT_PATH);
+        File[] listOfFiles = inputFilesFolder.listFiles();
+        for (File inputFile : listOfFiles) {
+            String fileName = inputFile.getName();
+            if (inputFile.isFile() && fileName.endsWith(INPUT_FILE_EXTENSION)) {
+                staffInputSolvers.add(new WizardOrderingSolver(inputFile));
+            }
+        }
+    }
     public static void main (String[] args) {
         start();
         solveAll();
+        startStaffFiles();
+        solveAllStaff();
     }
 }
--- src/main/java/com/wizardordering/WizardOrderingSolver.java
+++ src/main/java/com/wizardordering/WizardOrderingSolver.java
@@ -2,10 +2,14 @@ package main.java.com.wizardordering;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import edu.princeton.cs.algs4.Stopwatch;
+import org.junit.Assert;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,26 +27,46 @@ public class WizardOrderingSolver {
     private File inputFile;
     private String outFileName;
     private Set<String> wizardSet;
-    private int numWizards;
+    private List<int[]> clausesByConstraints;
+    private List<int[]> clausesByImplication;
+    private int[] assignments;
+
+    // For statistics
     private int numConstraints;
+    private double elapsedTime;
 
     /**
      * Constructor
+     * @param inputFile, the file from where the constraints will be read.
      */
     public WizardOrderingSolver(File inputFile) {
         this.solver = SolverFactory.newDefault();
         this.inputFile = inputFile;
         this.wizardSet = new HashSet<>();
-        this.preProcess();
+        this.clausesByConstraints = new ArrayList<>();
+        this.clausesByImplication = new ArrayList<>();
+        this.numConstraints = 0;
     }
 
+    /**
+     *  Returns file name.
+     * @return filename
+     */
+    public String getFileName() {
+        return this.inputFile.getName();
+    }
+
+    /**
+     * preProcess
+     * Reduces the wizard ordering problem to an instance of 3-SAT.
+     */
     public void preProcess() {
         this.generateVariables();
         this.generateConstraintClauses();
         this.generateImplicationClauses();
     }
 
-    /*
+    /**
      * Given two wizard IDs, return the corresponding variableID.
      * @param first, wizardID
      * @param second, wizardID
@@ -65,19 +89,20 @@ public class WizardOrderingSolver {
         return varID;
     }
 
-    /*
+    /**
      * Creates the set of wizard names mapped to a wizardID
      * Generates variables for each pair of wizard IDs, A and B, where A < B.
      * The variableID corresponding to (B, A) is denoted as -1 * variableID
-     * corresponding to (A, B)
+     * corresponding to (A, B).
+     * Moreover, +(A, B) == -(B, A) <=> age(A) < age(B)
+     *           -(A, B) == +(B, A) <=> age(A) > age(B)
      */
-
     private void generateVariables() {
         try {
             BufferedReader buf = new BufferedReader(new FileReader(this.inputFile));
 
             // Read number of wizards and constraints respectively.
-            this.numWizards = Integer.parseInt(buf.readLine().trim());
+            int numWizards = Integer.parseInt(buf.readLine().trim());
             this.numConstraints = Integer.parseInt(buf.readLine().trim());
 
 
@@ -85,14 +110,14 @@ public class WizardOrderingSolver {
                 StringTokenizer st = new StringTokenizer(buf.readLine());
                 while (st.hasMoreTokens()) {
                     wizardSet.add(st.nextToken());
-                    if (wizardSet.size() == this.numWizards)
+                    if (wizardSet.size() == numWizards)
                         break;
                 }
             }
 
             // Create mapping between wizard names and integer value.
             int wizId = 1;
-            this.wizIdToName = HashBiMap.create(this.numWizards);
+            this.wizIdToName = HashBiMap.create(numWizards);
             for (String name : wizardSet) {
                 this.wizIdToName.put(wizId++, name);
             }
@@ -100,9 +125,9 @@ public class WizardOrderingSolver {
             // Create mapping between variables and integer value.
             int varId = 1;
             //this.varIdToVar = HashBiMap.create(this.numWizards * (this.numWizards - 1) / 2);
-            this.varIdToWizID = HashBiMap.create(this.numWizards * (this.numWizards - 1) / 2);
-            for (int i = 1; i <= this.numWizards; i++) {
-                for (int j = i + 1; j <= this.numWizards; j++) {
+            this.varIdToWizID = HashBiMap.create(numWizards * (numWizards - 1) / 2);
+            for (int i = 1; i <= numWizards; i++) {
+                for (int j = i + 1; j <= numWizards; j++) {
                     List<Integer> lst = new ArrayList<>();
                     lst.add(i);
                     lst.add(j);
@@ -117,6 +142,11 @@ public class WizardOrderingSolver {
         }
     }
 
+    /**
+     * generateConstraintClauses
+     * For each constraint read from the input file:
+     *      Let a, b, c be the wizardIDs corresponding to the three names read, respectively.
+     */
     private void generateConstraintClauses() {
         try {
             BufferedReader buf = new BufferedReader(new FileReader(this.inputFile));
@@ -134,36 +164,100 @@ public class WizardOrderingSolver {
 
                 int a_c = getVariableIDbyWizard(a, c);
                 int b_c = getVariableIDbyWizard(b, c);
-
-                this.solver.addClause(new VecInt(new int[]{a_c, -1 * b_c}));
-                this.solver.addClause(new VecInt(new int[]{-1 * a_c, b_c}));
+                this.clausesByConstraints.add(new int[]{a_c, -1 * b_c});
+                this.clausesByConstraints.add(new int[]{-1 * a_c, b_c});
             }
+            Assert.assertEquals(this.numConstraints * 2, this.clausesByConstraints.size());
         } catch (IOException e) {
             System.out.println("Error reading constraints.");
             System.exit(-1);
+        }
+    }
+    /**
+     * For each trio of wizard IDs (i, j, k), there exists 3! = 6 valid orderings between them.
+     * They are as follows:
+     *      Variable(i, j) ^      Variable(j, k) =>      Variable(i, k)     Equation (1)
+     * -1 * Variable(i, j) ^ -1 * Variable(j, k) => -1 * Variable(i, k)     Equation (2)
+     *      Variable(i, j) ^ -1 * Variable(i, k) => -1 * Variable(j, k)     Equation (3)
+     * -1 * Variable(i, j) ^      Variable(i, k) =>      Variable(j, k)     Equation (4)
+     * -1 * Variable(i, k) ^      Variable(j, k) => -1 * Variable(i, j)     Equation (5)
+     *      Variable(i, k) ^ -1 * Variable(j, k) =>      Variable(i, j)     Equation (6)
+     * By boolean algebra, equations (1), (3), and (5) are equivalent to the CNF clause:
+     * -1 * Variable(i, j) v -1 * Variable(j, k) v       Variable(i, k)     Equation (7)
+     * Similarly, equations (2), (4), and (6) are equivalent to the CNF clause:
+     *      Variable(i, j) v      Variable(j, k) v  -1 * Variable(i, k)     Equation (8)
+     *
+     * Total of (N choose 3) * 2 = N * (N - 1) * (N - 2) / 3 clauses added.
+     */
+    private void generateImplicationClauses() {
+        int numWizards = wizardSet.size();
+        for (int i = 1; i <= numWizards; i++) {
+            for (int j = i + 1; j <= numWizards; j++) {
+                for (int k = j + 1; k <= numWizards; k++) {
+                    int i_j = getVariableIDbyWizard(i, j);
+                    int j_k = getVariableIDbyWizard(j, k);
+                    int i_k = getVariableIDbyWizard(i, k);
+
+                    this.clausesByImplication.add(new int[]{-1 * i_j, -1 * j_k, i_k});
+                    this.clausesByImplication.add(new int[]{i_j, j_k, -1 * i_k});
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds all clauses to the solver.
+     */
+    private void addAllClauses() {
+        try {
+            for (int[] clause : this.clausesByConstraints)
+                this.solver.addClause(new VecInt(clause));
+            for (int[] clause : this.clausesByImplication)
+                this.solver.addClause(new VecInt(clause));
         } catch (ContradictionException e) {
-            System.out.println("Error: Unsatisfiable clauses added.");
+            System.out.println("ERROR: Clauses contain contradiction!");
             System.exit(-2);
         }
     }
 
-    private void generateImplicationClauses() {
-        // TODO: Generate 3-SAT clauses based on transitivity property.
-        throw new UnsupportedOperationException();
-    }
-
+    /**
+     * Runs the solver.
+     */
     public void run() {
+        try {
+            Stopwatch watch = new Stopwatch();
+            addAllClauses();
 
+            IProblem problem = solver;
+
+            if (problem.isSatisfiable()) {
+                this.elapsedTime = watch.elapsedTime();
+                this.assignments = problem.model();
+                System.out.println("File " + this.getFileName() + " has been solved!!");
+            }
+        } catch (TimeoutException e) {
+            System.out.println("Error: Timeout exception.");
+            System.exit(-3);
+        }
     }
 
+    /**
+     * PostProcessing method.
+     * Builds a Directed Acyclic Graph and topological sorts the wizards.
+     */
     public void postProcess() {
-        // TODO: Create a DAG based on assignments by solver.
-        throw new UnsupportedOperationException();
+        this.printStatistics();
     }
 
+    /**
+     * Prints useful statistics.
+     */
     public void printStatistics() {
         System.out.println("File name: " + this.inputFile.getName());
         System.out.println("Number of wizards: " + this.wizardSet.size());
         System.out.println("Number of variables: " + this.varIdToWizID.size());
+        System.out.println("Number of clauses by constraints: " + this.clausesByConstraints.size());
+        System.out.println("Number of clauses by implication: " + this.clausesByImplication.size());
+        System.out.println("Running time: " + this.elapsedTime + "s\n");
     }
 }
